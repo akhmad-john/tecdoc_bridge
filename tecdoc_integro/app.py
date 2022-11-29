219ec63984fcaa01c23d0b60e61c4eb184b956b0
Akhmadjon Shamsuddinov <a.shamsuddinov@clbr.uz>
2022-11-30 00:59:57 +0500

    suppliers adoptation8

--- tecdoc_integro/app.py
+++ tecdoc_integro/app.py
@@ -7,7 +7,7 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-# api = Api(app)
+api = Api(app)
 # session = load_session()
 
 @app.route("/")
@@ -40,22 +40,16 @@ def hello_world():
 #         }
 
 
-# @api.route('/main')
-# class SuppliersAPI(Resource):
-#     def get(self):
-#         suppliers = filter_queryset_first(session, Suppliers, 10)
-#         suppliers_list = []
-#         for supplier in suppliers:
-#
-#             suppliers_list.append({
-#                 'dataversion': supplier.dataversion,
-#                 'description': supplier.description,
-#
-#             })
-#         return {
-#             "message": "Hello",
-#             "articles": suppliers_list
-#         }
+@api.route('/main')
+class SuppliersAPI(Resource):
+    def get(self):
+
+        suppliers_list = [
+            {'name': "Article 1"},
+            {'name': "Article 2"}
+        ]
+
+        return suppliers_list
 
 if __name__ == '__main__':
     app.run()
